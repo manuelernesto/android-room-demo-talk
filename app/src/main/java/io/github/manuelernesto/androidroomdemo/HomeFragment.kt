e8b45e7bea9014e163f84092ceb96027ffb412f0
manuelernesto <manuelernesto@outlook.com>
2020-10-10 15:02:45 +0100

    design the UI

--- app/src/main/java/io/github/manuelernesto/androidroomdemo/HomeFragment.kt
+++ app/src/main/java/io/github/manuelernesto/androidroomdemo/HomeFragment.kt
@@ -21,7 +21,7 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
+        view.findViewById<Button>(R.id.button_start).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
