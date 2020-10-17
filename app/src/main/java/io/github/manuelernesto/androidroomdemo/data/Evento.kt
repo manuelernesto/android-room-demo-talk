f9f1d68f5acc0bca3cde22cacc81e9a308fd650e
manuelernesto <manuelernesto@outlook.com>
2020-10-17 19:37:00 +0100

    refactoring

--- app/src/main/java/io/github/manuelernesto/androidroomdemo/data/Evento.kt
+++ app/src/main/java/io/github/manuelernesto/androidroomdemo/data/Evento.kt
@@ -1,2 +1,17 @@
 package io.github.manuelernesto.androidroomdemo.data
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.io.Serializable
+
+@Entity(tableName = "evento")
+data class Evento(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "codigo")
+    var id: Long = 0,
+    val titulo: String,
+    val descricao: String,
+    val palestrante: String,
+    val data: Long
+) : Serializable
--- app/src/main/java/io/github/manuelernesto/androidroomdemo/ui/NewFragment.kt
+++ app/src/main/java/io/github/manuelernesto/androidroomdemo/ui/NewFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.navigation.Navigation
 import io.github.manuelernesto.androidroomdemo.R
+import io.github.manuelernesto.androidroomdemo.data.Evento
+import io.github.manuelernesto.androidroomdemo.data.EventoDB
 import io.github.manuelernesto.androidroomdemo.util.StandardFragment
 import kotlinx.android.synthetic.main.fragment_new.*
+import kotlinx.coroutines.launch
 import java.util.*
 
 
@@ -51,7 +55,20 @@ class NewFragment : StandardFragment() {
     }
 
     private fun save() {
+        launch {
+            context?.let {
+                val evento = Evento(
+                    titulo = ed_nome.text.toString(),
+                    descricao = et_descricao.text.toString(),
+                    palestrante = et_palestrante.text.toString(),
+                    data = mData!!
+                )
 
+                EventoDB.getDatabase(it).dao().salvar(evento)
+                Toast.makeText(it, "Evento Salvo!", Toast.LENGTH_LONG).show()
+                cleanField()
+            }
+        }
     }
 
     private fun delete() {
